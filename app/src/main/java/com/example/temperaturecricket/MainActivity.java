55524c98c5fe9ce30cf59648efd15299974d9be0
Yadav Satyanand <satyanandyadav1517@gmail.com>
2021-09-21 11:37:59 +0530

    added the popup feature

--- app/src/main/java/com/example/temperaturecricket/MainActivity.java
+++ app/src/main/java/com/example/temperaturecricket/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.temperaturecricket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextNumber;
+    Button button;
+    TextView output;
+    DecimalFormat formatter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextNumber=findViewById(R.id.editTextNumber);
+        button=findViewById(R.id.button);
+        output=findViewById(R.id.output);
+         formatter=new DecimalFormat("#0.00");
+        output.setVisibility(View.GONE);
+        button.setOnClickListener(v -> {
+
+            String numberOfChirps=editTextNumber.getText().toString().trim();
+            if(numberOfChirps.isEmpty())
+            {
+                Toast.makeText(MainActivity.this,"fill the number of chirps",Toast.LENGTH_SHORT).show();
+            }
+            else {
+                int value=Integer.parseInt(numberOfChirps);
+                double degree=(value/3.0)+4;
+                String put="The approximate temperature of outside is "+formatter.format(degree)+" degrees Celsius.";
+                output.setText(put);
+                output.setVisibility(View.VISIBLE);
+            }
+
+        });
+
     }
 }
